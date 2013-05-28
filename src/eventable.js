29d857ff91e6273760e3b8c949a235e6b573c6c0
creativej <lu.jamison@gmail.com>
2013-05-28 19:45:11 +1000

    Adding IE support for indexOf

--- src/eventable.js
+++ src/eventable.js
@@ -1,6 +1,19 @@
 (function(window) {
 	'use strict';
 
+	// Add ECMA262-5 Array methods if not supported natively
+	if (!Array.prototype.indexOf) {
+		Array.prototype.indexOf = function(find, i /*opt*/) {
+			if (i === undefined) { i = 0; }
+			if (i < 0) { i += this.length; }
+			if (i < 0) { i = 0; }
+			for (var n = this.length; i < n; i++) {
+				if (i in this && this[i] === find) { return i; }
+			}
+			return -1;
+		};
+	}
+
 	window.eventable = function(obj) {
 		var events = {};
 		obj = obj || {};
